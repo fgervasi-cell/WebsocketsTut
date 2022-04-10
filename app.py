9ec6cb1a2692a3101f209f1986d3171a01b45b6c
fgervasi-cell <gervasi.felix@student.dhbw-karlsruhe.de>
2022-04-10 16:52:32 +0200

    Deploy to Heroku.

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import asyncio
 import json
 import secrets
+import os
+import signal
 
 import websockets
 from connect4 import PLAYER1, PLAYER2
@@ -132,8 +134,14 @@ async def play(websocket, game, player, connected):
 
 
 async def main():
-    async with websockets.serve(handler, "", 8001):
-        await asyncio.Future()  # run forever
+    # Set the stop condition when receiving SIGTERM.
+    loop = asyncio.get_running_loop()
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+
+    port = int(os.environ.get("PORT", "8001"))
+    async with websockets.serve(handler, "", port):
+        await stop
 
 
 if __name__ == "__main__":
